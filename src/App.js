a4de62034ea08ddd707ad284b93236148ffadb29
Vadim Semenets <semvadser@gmail.com>
2019-02-10 13:22:19 +0200

    testReact

--- src/App.js
+++ src/App.js
@@ -1,28 +1,122 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom'
+import Loader from 'react-loader-spinner'
+import { connect } from 'react-redux'
+import Flag from 'react-world-flags'
+
+import { addCity, removeCity } from './actions'
+import { API_KEY } from './contants'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		isDataLoading: false,
+		cityNotFound: false 
+	}
+
+	constructor(props) {
+		super(props)
+
+		const geoSuccess = position => {
+			this.addCityByCoordinates(position.coords.latitude, position.coords.longitude)
+		}
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(geoSuccess);
+		}
+	}
+
+	addCityByCoordinates = async (lat, lon) => {
+		const { cities, dispatch } = this.props
+
+		const api_url = await
+			fetch (`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+		const data = await api_url.json();
+
+		if (cities.find(city => city.name.toLowerCase() === data.name.toLowerCase())) {
+			return null
+		}
+
+		dispatch(addCity(data.name, data.sys.country))
+	}
+
+	addCityByName = async () => {
+		const { cities, dispatch } = this.props
+
+		const cityName = this.input.value
+		if (!cityName) return null
+
+		if (cities.find(city => cityName.toLowerCase() === city.name.toLowerCase())) {
+			return null
+		}
+
+		this.setState({ isDataLoading: true })
+
+		const result = await
+			fetch (`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
+		
+		if (result.status === 404 ) {
+			return this.setState({
+				cityNotFound: true,
+				isDataLoading: false,
+			})
+		} 
+
+		const data = await result.json();
+
+		dispatch(addCity(data.name, data.sys.country))
+		
+		this.setState({
+			isDataLoading: false,
+			cityNotFound: false,
+		})
+	}
+
+	removeCity = city => {
+		this.props.dispatch(removeCity(city.name))
+	}
+
+	render() {
+		const { isDataLoading, cityNotFound } = this.state
+		const { cities } = this.props
+
+		return (
+			<div>
+				<h1 className="page-title">Погода</h1>
+				<div className="city-input">
+					<input type="text" name="city" placeholder="Введите город, например Warsaw или Kiev" ref={input => this.input = input} />
+					<button onClick={this.addCityByName}>Добавить город</button>
+					
+					<div className="error">{cityNotFound && "Город не найден!"}</div>
+				</div>
+				<div className="cities">
+					{cities.map(city => {
+						return (<div className="city" key={city.name}>
+							<Link to={`city/${city.name}`}>
+								<div className="name">{city.name}</div>
+								<Flag code={ city.country } height={16}/>
+							</Link>
+							<button onClick={() => this.removeCity(city)}>Удалить</button>
+						</div>
+						)
+					})}
+				</div>
+				<div className="spinner">
+					{isDataLoading && <Loader
+						type="Circles"
+						color="#4051d4"
+						height="50"	
+						width="50"
+					/>}  
+				</div>
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapStateToProps = state => ({
+	cities: state
+})
+
+export default connect(mapStateToProps)(App)
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route } from "react-router-dom"
+import createBrowserHistory from "history/createBrowserHistory"
+
 import './index.css';
 import App from './App';
+import City from './City'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux"
+import { createStore, compose } from "redux"
+import persistState from 'redux-localstorage'
+
+import rootReducer from "./reducer"
+
+const enhancer = compose(
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+	persistState(),
+)
+
+const store = createStore(rootReducer, enhancer)
+const history = createBrowserHistory();
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={history}>
+			<React.Fragment>
+				<Route exact path="/" component={App}/>
+				<Route exact path="/city/:cityName" component={City} />
+			</React.Fragment>
+		</Router>
+	</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
